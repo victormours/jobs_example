480040f90976f9baa97eca1e209891beef4d60c0
Victor Mours <victor.mours@gmail.com>
2017-12-13 23:15:55 +0100

    Install twitter bot

--- Gemfile
+++ Gemfile
@@ -17,6 +17,9 @@ gem 'simple_form'
 gem 'uglifier'
 gem 'webpacker'
 
+gem 'twitter'
+gem 'faker'
+
 group :development do
   gem 'web-console', '>= 3.3.0'
 end
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,22 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: [:home, :tweet]
 
   def home
   end
+
+  def tweet
+    client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = ENV["CONSUMER_KEY"]
+      config.consumer_secret     = ENV["CONSUMER_SECRET"]
+      config.access_token        = ENV["ACCESS_TOKEN"]
+      config.access_token_secret = ENV["ACCESS_SECRET"]
+    end
+
+    time = I18n.localize(Time.now, format: "%H:%H:%S")
+    client.update("It's #{time}, and some day I'll visit #{Faker::Address.street_name}.")
+
+    flash[:notice] = "You just tweeted something!"
+    redirect_to root_path
+  end
+
 end
